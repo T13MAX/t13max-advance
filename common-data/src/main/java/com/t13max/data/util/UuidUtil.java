0421add2b945c3d8eaab4f88eec2b1e247a7a2c2
傻呆呆-HOME <t13max@163.com>
2024-08-20 08:24:59 +0800

    fix: uuid优化

--- common-data/src/main/java/com/t13max/data/util/UuidUtil.java
+++ common-data/src/main/java/com/t13max/data/util/UuidUtil.java
@@ -13,25 +13,27 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @UtilityClass
 public class UuidUtil {
-    private static long START_TIME = 1716912000000L;//2024-05-29 00:00:00
+    private final static long START_TIME = 1716912000000L;//2024-05-29 00:00:00
 
-    private final AtomicLong ID = new AtomicLong();
+    private final static AtomicLong ID = new AtomicLong();
 
-    private static int MASK_12 = 0XFFF;//12位掩码
+    private final static int MASK_12 = 0XFFF;//12位掩码
 
-    private static int INSTANCE_NO;
+    private final static int INSTANCE_NO;
 
     static {
         BaseConfig config = Application.config();
         if (config != null) {
             INSTANCE_NO = Application.config().getInstanceNo();
         } else {
+            INSTANCE_NO = 0;
             Log.data.error("config为空, instanceNo为0");
         }
     }
 
     /**
      * 全局唯一
+     * 后续优化一下 目前有那么一丢丢的可能就是一毫秒内获取超过4096个id就出事了
      *
      * @Author t13max
      * @Date 11:11 2024/4/16
