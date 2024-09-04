b0e268920501c27e91e72d593f2fdf937c13484a
t13max-x <t13max@163.com>
2024-09-04 13:44:18 +0800

    feat: 任务

---
+++ service-game/src/main/java/com/t13max/game/feature/quest/QuestMemory.java
@@ -0,0 +1,14 @@
+package com.t13max.game.feature.quest;
+
+import com.google.protobuf.MessageLite;
+import com.t13max.game.memory.MapMemory;
+
+/**
+ * 任务
+ *
+ * @author t13max
+ * @since 13:42 2024/9/4
+ */
+public class QuestMemory {
+
+}
