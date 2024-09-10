fe7022966aa2c0cce0c07fa37cdc52fcebbaf499
t13max-x <t13max@163.com>
2024-09-10 15:48:40 +0800

    feat: 聊天管理器

---
+++ service-social/src/main/java/com/t13max/social/chat/ChatManager.java
@@ -0,0 +1,14 @@
+package com.t13max.social.chat;
+
+import com.t13max.common.manager.ManagerBase;
+
+/**
+ * 聊天管理器
+ *
+ * @author t13max
+ * @since 15:44 2024/9/10
+ */
+public class ChatManager extends ManagerBase {
+
+
+}
